a153eef805d8ee48c6aac602952be5db8e2fff18
vishu8474 <vishalgupta0502462@gmail.com>
2025-11-22 14:01:33 +0530

    Update CORS middleware to include Vercel domain
    Added Vercel domain to CORS origin list and specified allowed methods and headers.

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:5173', 'http://localhost:5174'],
-  credentials: true
+  origin: [
+    'http://localhost:8080', 
+    'http://localhost:5173', 
+    'http://localhost:5174',
+    'https://prokhoz-frontend.vercel.app' // Add your Vercel domain here
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
